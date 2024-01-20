817b44d240d75a48948dbaa59b0815e777dc2480
blackNumberThree <bid199376@gamil.com>
2024-01-20 11:53:39 +0200

    add css

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
 import { JSXComponent } from "./JSXomponent";
 import { FunctionalComponent } from "./FunctionalComponent";
 import { ClassComponent } from "./ClassComponent";
-
+import "../index.css";
 function App() {
   return (
     <>
