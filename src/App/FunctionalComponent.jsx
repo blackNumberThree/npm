4cb48ac675043cc7780762bdf72114469639368f
blackNumberThree <bid199376@gamil.com>
2024-01-27 12:02:16 +0200

    add wrong state

--- src/App/FunctionalComponent.jsx
+++ src/App/FunctionalComponent.jsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 
-export function FunctionalComponent() {
+export function FunctionalComponent(props) {
+  let number = 1;
+  // console.log(props);
   // const ref = useRef(0);
   // const handleSubmit = (event) => {
   /* Скасуємо реакцію оброблювача за замовченням */
@@ -10,9 +12,16 @@ export function FunctionalComponent() {
   // let fName = ref.current.value;
   // console.log(fName);
   // };
+  function iterator() {
+    number = number + 1;
+  }
 
   return (
-    <p>Hello i am functional Component</p>
+    <>
+      <p>Hello i am functional Component</p>
+      <p>{number}</p>
+      <button onClick={iterator}>Press me</button>
+    </>
     // <form onSubmit={handleSubmit}>
     //   <input type="text" id="userName" ref={ref} />
     //   <input type="submit" value="Click me" />
